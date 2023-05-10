919c1c19728fd3d526d6d942d68c14cbd2fa704e
sanjanvadi <sanjanvadi@gmail.com>
2023-05-10 17:35:54 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import NotFound from './components/NotFound';
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:4000'
+    uri: '/graphql/'
   })
 });
 
